ef8a41eee4e8914768dcef3240857a310220a6e9
owncilck <79367161+owncilck@users.noreply.github.com>
2021-04-20 19:56:07 +0900

    a
    a

---
+++ PHP/return/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP入門2</title>
+	</head>
+	<body>
+		<?php 
+			$score = $_POST["id"];
+			print $score * 2;
+		
+		?>
+		<a href="/PHP/home">戻る</a>
+	</body>
+</html>
---
+++ PHP/test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP入門</title>
+	</head>
+	<body>
+		<form method='post' action='test2.php'>
+		<input type='text' name='id' placeholder='入力してください'>
+		<input type='submit' value='送信'>
+		</form>
+
+	</body>
+</html>
